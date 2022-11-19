ee86b6b2dd01fe64afa8567d4edfad90030249b1
deliaberes <114994437+deliaberes@users.noreply.github.com>
2022-11-19 21:18:21 +0100

    Add files via upload

---
+++ atlos.py
@@ -0,0 +1,26 @@
+# File: autokoltseg.py
+# Author: Béres Délia
+# Copyright: 2022, Béres Délia
+# Group: Szoft 1/I/E
+# Date: 2022-11-19
+# Github: https://github.com/deliaberes
+# Licenc: GNU GPL
+ 
+
+print("Béres Délia")
+print("2022.11.19")
+print("SZOFT 1/I/E")
+print(" ")
+
+
+import math
+
+a = float(input("Adja meg az 'a' oldalt: "))
+b = float(input("Adja meg az 'b' oldalt: "))
+print(" ")
+
+a2 = (a**2)
+b2= (b**2)
+d = math.sqrt(a2+b2)
+print("Az átló értéke: ", d)
+
